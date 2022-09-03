d1d3684293d8594b3bee95cb1c141bf5bc973d9c
yeon <yeon8747@naver.com>
2022-09-03 21:52:52 +0900

    2914번.kt

---
+++ BackJoon/Kotlin/2914.kt
@@ -0,0 +1,11 @@
+```kotlin
+import java.util.Scanner
+fun main(argsg: Array<String>){
+	val sc: Scanner = Scanner(System.`in`)
+
+	var a = sc.nextInt()
+  var b = sc.nextInt()
+  println(a*(b - 1)+ 1)
+
+}
+```
