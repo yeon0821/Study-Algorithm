b78fd6379410ce40778655e89ac7924170198907
Yeonu <102429539+yeon0821@users.noreply.github.com>
2023-01-03 09:05:57 +0900

    2577.java

---
+++ BackJoon/Java/2577.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int value = (in.nextInt() * in.nextInt() * in.nextInt());
+		String str = Integer.toString(value);
+		in.close();
+		
+		for (int i = 0; i < 10; i++) {
+			int count = 0;
+			for (int j = 0; j < str.length(); j++) {
+				if ((str.charAt(j) - '0') == i) {
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
+	}
+}
