f5297057fb58ab6b5ff7a1e442eff48a39d3f846
김연우 <102429539+yeon0821@users.noreply.github.com>
2023-11-20 22:13:41 +0900

    10773.kt

---
+++ BackJoon/Kotlin/10773.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val case = readLine()!!.toInt()
+
+    var array = ArrayList<Int>()
+
+    for(i in 0 until case) {
+        val read = readLine()!!.toInt()
+        if(read == 0){
+            array.removeLast()
+        } else {
+            array.add(read)
+        }
+    }
+    print(array.sum())
+}
